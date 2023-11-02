c65ca26cda0d56f5ade1ccbf8d8b0437fc6d6af3
“AirJailer” <airjailer1@gmail.com>
2023-11-02 07:03:04 -0700

    l

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-sum = 0
-for args in sys.argv[1:]:
-    sum += int(args)
-print(sum)
+    sum = 0
+    for args in sys.argv[1:]:
+        sum += int(args)
+    print(sum)
