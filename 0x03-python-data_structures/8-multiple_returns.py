d76afe8ca2c9c42511aa39ef68158d2476fdd502
“AirJailer” <airjailer1@gmail.com>
2023-11-05 08:32:59 -0800

    m

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    len_str = len(sentence)
-    first = sentence[0]
-    return (len_str, first if len_str > 0 else None)
+    if len(sentence) > 0 :
+        return(len(sentence), sentence[0])
+    else:
+        return None
+        
