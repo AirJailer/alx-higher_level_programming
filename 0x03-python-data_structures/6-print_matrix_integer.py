3ba0534b1d27b8ba0e679942eb5de31b526a18b4
“AirJailer” <airjailer1@gmail.com>
2023-11-05 15:43:33 -0800

    m

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,5 +6,4 @@ def print_matrix_integer(matrix=[[]]):
         if len(i) == 0:
             print()
         for j in range(len(i)):
-            print("{}".format(i[j]), end="\n" if j is 
-                  len(i) - 1 else " ")
+            print("{:d}".format(i[j]), end="\n" if j is len(i) - 1 else " ")
