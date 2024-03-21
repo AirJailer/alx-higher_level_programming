bed2fa26705aadf3e4596bdfaf159168b30027e6
AirJailer <airjailer1@gmail.com>
2024-03-20 19:41:07 -0700

    m

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    updated_dict = a_dictionary.copy()
-    updated_dict[key] = value
-    return updated_dict
+    a_dictionary[key] = value
+    return a_dictionary
